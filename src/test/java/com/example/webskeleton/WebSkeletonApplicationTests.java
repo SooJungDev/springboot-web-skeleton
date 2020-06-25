856c4767ac3429cb1a0a19fa367c496f3953b2a1
crystal <crystal2840@neowiz.com>
2020-06-25 18:28:51 +0900

    :fire: 프로젝트 초기 세팅

---
+++ src/test/java/com/example/webskeleton/WebSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
